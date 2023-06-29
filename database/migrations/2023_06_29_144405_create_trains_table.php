be3ff7fa714ab4403f6ef9627fdcda822fda4063
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-06-29 18:19:35 +0200

    esercizio completo

--- database/migrations/2023_06_29_144405_create_trains_table.php
+++ database/migrations/2023_06_29_144405_create_trains_table.php
@@ -15,14 +15,18 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string("Azienda", 100)->nullable();
-            $table->string("Stazione di partenza", 100)->nullable();
-            $table->string("Stazione di arrivo", 100)->nullable();
-            $table->dateTime("Orario di partenza")->nullable();
-            $table->dateTime("Orario di arrivo")->nullable();
-            $table->integer("Numero Carrozze")->nullable();
-            $table->integer("In orario")->nullable();
-            $table->boolean("Cancellato")->nullable();
+            $table->string("azienda", 100)->nullable();
+
+            $table->string("stazione_di_partenza", 100)->nullable();
+            $table->string("stazione_di_arrivo", 100)->nullable();
+            $table->char('Codice_treno', 10);
+            $table->date("data_di_partenza")->nullable();
+            $table->dateTime("orario_di_partenza")->nullable();
+            $table->dateTime("orario_di_arrivo")->nullable();
+            $table->integer("numero_Carrozze")->nullable();
+            $table->integer("in_orario")->nullable();
+            $table->boolean("cancellato")->nullable();
+            $table->timestamps();
 
         });
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container my-3">
-    <h1>Welcome Page</h1>
-    <div class="row g-4">
-        <div class="col">
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, delectus ad esse illum omnis earum eligendi sint a minus quasi, inventore nulla autem. Maxime voluptatem eligendi veniam voluptates. Soluta, sunt!</p>
-            </div>
+<div class="container-fluid bg-warning">
+    <div class="container py-3">
+        <div class="row g-4">
+            @foreach ($trains as $train)
+                @if ($train->data_di_partenza == "2023-06-29")
+                    <div class="col-3 ">
+                        <div class="card p-3 bg-success">
+                            <p>Codice treno: {{$train->Codice_treno}}</p>
+                            <h4>Compagnia: {{$train->azienda}}</h4>
+                            <p>Stazione di partenza: {{ $train->stazione_di_partenza }}</p>
+                            <p>Orario di partenza: {{ $train->orario_di_partenza }}</p>
+                            <p>Stazione di partenza: {{ $train->stazione_di_arrivo }}</p>
+                            <p>Orario di partenza: {{ $train->orario_di_arrivo }}</p>
+                            @if ($train->in_orario > 0)
+                            <p>Ritardo: {{ $train->in_orario }}</p>
+                            @endif
+                            @if ($train->cancellato == 1)
+                            <p>Stato: Cancellato</p>
+                            @endif
+                        </div>
+                    </div>
+                @endif
+            @endforeach
         </div>
     </div>
-
 </div>
 @endsection
